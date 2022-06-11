1b0743326089d95b18278a2811d34ce1a4878d0c
Alexandre DESIR <alexandre.desir@icloud.com>
2022-06-11 14:38:18 -0400

    error serializing

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,12 +1,45 @@
 import Image from 'next/image'
 import React from 'react'
 
-const DetailsProduct = (context) => {
+
+const DetailsProduct = (props) => {
+  console.log(props.params)
   return (
     <div>
-    <Image src={} width={300} height={300} />
+    DetailsProduct
     </div>
   )
+}
+
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const products = await res.json();
+    const params = context.params.id;
+    const productId = products.find((product) => product.id === params);
+    return {
+      props: {
+        params: productId,
+      },
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+}
+
+
+export async function getStaticPaths (){
+  const res = await fetch('https://fakestoreapi.com/products')
+  const products = await res.json()
+  const ids = products.map(product => ({ params:{ id : product.id.toString()}}) )
+
+  return {
+    paths: ids,
+    fallback: 'blocking'
+  }
 }
 
 export default DetailsProduct
