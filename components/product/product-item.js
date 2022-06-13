873582f3e74c64e7e6678c2fcc45d142edb84601
Alexandre DESIR <alexandre.desir@icloud.com>
2022-06-13 07:51:35 -0400

    router bug fixed"

--- components/product/product-item.js
+++ components/product/product-item.js
@@ -3,9 +3,18 @@ import Image from "next/image";
 import style from "./product-item.module.css";
 import { Rating } from 'react-simple-star-rating'
 import { Button,ButtonLink } from "../button/button";
+import { useRouter } from "next/router";
 
 const Product = ({ cover, title, price, rating, id }) => {
-  const detailsProduct = `\products/${id}`;
+  let detailsProduct = `/products/${id}`;
+
+    const router = useRouter();
+    if (router.asPath === detailsProduct){
+      return(
+        detailsProduct = `${id}`
+      )
+    }
+
   return (
     <li className={style.container}>
       <Image src={cover} alt={title} width={200} height={200} className={style.image} layout={'intrinsic'} />
